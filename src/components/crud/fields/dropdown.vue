5c1577d6be5ba005b5c5b8b3d2a5e117ec0f8341
Asad Mansuri <asad.mansuri@matrixhive.com>
2023-11-16 18:23:08 +0530

    options is not empty check added

--- src/components/crud/fields/dropdown.vue
+++ src/components/crud/fields/dropdown.vue
@@ -28,7 +28,7 @@
         </div>
       </li>
 
-      <li>
+      <li v-if="options.length > 0">
         <div class="flex items-center p-2 rounded hover:bg-gray-100 dark:hover:bg-gray-600">
           <input type="checkbox"
             class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"
