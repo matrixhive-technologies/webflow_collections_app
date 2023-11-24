df9e0a688041659f08bf32a31bd59aedb7fa0b27
Asad Mansuri <asad.mansuri@matrixhive.com>
2023-11-24 14:38:09 +0530

    unwanted code removed

--- app/image.php
+++ app/image.php
@@ -303,39 +303,4 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
             echo json_encode(['code' => 400]);
         }
     }
-
-    // Output image file
-    // $targetFolder   = 'uploads/';
-    // $targetFilename = $targetFolder . uniqid() . '.jpg';
-
-    // // Desired crop dimensions
-    // $cropWidth  = $_REQUEST['width'];
-    // $cropHeight = $_REQUEST['height'];
-
-    // // Load the image
-    // $sourceImage = imagecreatefromjpeg($inputImagePath);
-
-    // // Get the current dimensions of the image
-    // $sourceWidth = imagesx($sourceImage);
-    // $sourceHeight = imagesy($sourceImage);
-
-    // // Calculate the crop position (centered in this example)
-    // $cropX = ($sourceWidth - $cropWidth) / 2;
-    // $cropY = ($sourceHeight - $cropHeight) / 2;
-
-    // // Create a new image with the desired crop dimensions
-    // $croppedImage = imagecreatetruecolor($cropWidth, $cropHeight);
-
-    // // Perform the crop
-    // imagecopy($croppedImage, $sourceImage, 0, 0, $cropX, $cropY, $cropWidth, $cropHeight);
-
-    // // Save the cropped image
-    // chmod($targetFolder, '0777');
-    // imagejpeg($croppedImage, $targetFilename);
-
-    // // Free up memory
-    // imagedestroy($sourceImage);
-    // imagedestroy($croppedImage);
-
-    // echo 'Cropped image saved  and uploaded successfully to ' . $targetFilename;
 }
